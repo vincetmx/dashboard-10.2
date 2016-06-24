12eafa31ede6934c9b7dfdb489f21dbd406e164f
pinkpanther24 <preetisdayout@gmail.com>
2016-06-24 17:16:56 -0400

    work so far

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -2,3 +2,8 @@
 // LOGIN CONTROLLER
 // Description: Define the following functionalities:
 // Making service calls to login a user
+  dashBoard.controller("loginCtrl",function($scope,authenticateService){        
+            $scope.submit = function(){
+            authenticateService.returnMessage("minh","faith");
+            } 
+});
--- app/scripts/services/authenticate.js
+++ app/scripts/services/authenticate.js
@@ -1,3 +1,20 @@
 'use strict';
 // AUTHENTICATE SERVICE
 // Description: Define the authenticateService that has 3 functionalities: login, logout, and islogged 
+dashBoard.service('authenticateService', function($rootScope,$http,$location) {
+        $rootScope.loginIn=false;
+        this.returnMessage= function(username,password){
+            $http({
+            url: '/api/login',
+            method: 'POST',
+            data: {"userName":username,"password":password}
+        }
+        )
+        .success(function(){ 
+        $rootScope.loginIn=true;
+       $location.path('/root');})
+        .error(function(){alert("wrong password")});
+        }
+    });
+
+[2:31
