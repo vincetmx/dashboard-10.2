094477001214a6e97662017d766f7d593f849e11
monkeybits1 <jcjulia.chu@gmail.com>
2016-06-29 20:54:51 -0400

    FINISHED: gulp task assignment. Make sure to 'npm install' 'package.json', if you want to play with 'gulpfile.js' . Comman lines are:  'gulp sass', 'gulp imagemin', 'gulp clean', 'gulp jshint', and 'gulp watch' FYI , test1.scss and test2.scss are just for testing purpose.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ var gulp = require ('gulp');
 //require other packages
 var sass= require('gulp-sass');
 var autoprefixer = require ('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+var changed = require('gulp-changed');
+var clean = require('gulp-clean');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
 
 
 //default task, currently will do nothing
@@ -25,3 +31,42 @@ gulp.task('sass',function(){
 
 
 
+//second task: image minify
+gulp.task('imagemin', function(){
+    return gulp.src('./app/assets/images/src/*/*.*')
+    .pipe(imagemin({
+ 
+    }))
+    //only for the ones that are
+//changing from the destination folder
+    .pipe(changed('./app/assets/images/build/'))  
+    .pipe(gulp.dest('./app/assets/images/build/'))
+});
+
+
+
+//third tastk: clean the folder
+gulp.task ('clean', function(){
+    return gulp.src('./app/assets/images/build', {read:false})
+    .pipe(clean());
+});
+
+
+
+//fourth task: JS stylish reporters
+gulp.task('jshint', function(){
+    return gulp.src('./app/scripts/*/*.js')
+    .pipe(jshint())
+//with stylish reporters
+    .pipe(jshint.reporter(stylish))
+// break the task when there is error
+    .pipe(jshint.reporter('fail'))
+});
+
+
+
+//fifth task: watch
+gulp.task('watch',function(){
+    gulp.watch('./app/assets/sass/*.scss',['sass']);
+    gulp.watch('./app/assets/images/src/*/*.*',['imagemin']);
+});
