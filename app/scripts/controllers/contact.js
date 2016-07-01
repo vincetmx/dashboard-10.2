f440d53fc18b6596127c41680751d3791893fc09
oliverljl <oliverljl@hotmail.com>
2016-07-01 10:35:00 -0400

    contact

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -2,3 +2,43 @@
 // CONTACT CONTROLLER
 // Description: Define the following functionalities:
 // All the functionalities on the "Contact" section for the form
+dashBoard.controller("contactCtrl", function($scope, contactService) {
+            contactService.getLocation()
+            .then(
+        	function mySuccess(response) {
+              	$scope.locations= response.data.locations;
+              	$scope.selected = $scope.locations[0];
+               },
+        function myError() {
+            console.log("ERROR");
+        });
+            contactService.getCategory()
+            .then(
+          function mySuccess(response) {
+                $scope.categories= response.data.categories;
+                $scope.selected = $scope.categories[0];
+               },
+        function myError() {
+            console.log("ERROR");
+        });
+            $scope.currentTime = new Date();
+  });
+
+dashBoard.controller('RatingDemoCtrl', function ($scope) {
+  $scope.rate = 7;
+  $scope.max = 10;
+  $scope.isReadonly = false;
+
+  $scope.hoveringOver = function(value) {
+    $scope.overStar = value;
+    $scope.percent = 100 * (value / $scope.max);
+  };
+
+  $scope.ratingStates = [
+    {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+    {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+    {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+    {stateOn: 'glyphicon-heart'},
+    {stateOff: 'glyphicon-off'}
+  ];
+});
--- app/scripts/services/getContact.js
+++ app/scripts/services/getContact.js
@@ -1,3 +1,17 @@
 'use strict';
 // GETCONTACT SERVICE
 // Description: Define the contactService that has 2 functionalities: getLocation and getCategory 
+dashBoard.service('contactService', function($http) {
+    this.getLocation = function() {
+        return $http({
+            url: '/api/location',
+            method: 'GET'
+        })
+    }
+    this.getCategory = function() {
+        return $http({
+            url: '/api/category',
+            method: 'GET'
+        })
+    }
+});
