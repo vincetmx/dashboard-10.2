da9db981cdd6322b67d417b9b3cee254761ff8b5
pinkpanther24 <preetisdayout@gmail.com>
2016-07-05 15:12:03 -0400

    contact.css retreived

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -8,29 +8,34 @@ gulp.task('default', function() {
 
 });
 
+//sass task
 gulp.task('sass', function() {
     return gulp.src('./assets/sass/*.scss')
         .pipe(sasscompile())
         .pipe(gulp.dest('./assets/styles/'));
 });
 
+//imagemin task
 gulp.task('imagemin', function() {
     return gulp.src('./assets/images/src/**/*')
         .pipe(imgmin())
         .pipe(gulp.dest('./assets/images/build/'));
 });
 
+//clean task
 gulp.task('clean', function(){
 	return gulp.src('./assets/images/build/', {read: false})
 		.pipe(clean());
 });
 
+//jshint task
 gulp.task('jshint',function(){
 	 return gulp.src(['./scripts/*.js','./scripts/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish', {beep: true}));
 });
 
+//watch task
 gulp.task('watch', function() {
   gulp.watch('./scripts/*.js', ['jshint']);
   gulp.watch('./scripts/**/*.js', ['jshint']);
